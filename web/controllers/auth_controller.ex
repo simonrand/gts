86430a1b2fad0f9e2b9811b8274a3fe48a36fec9
Simon Rand <simon@kitmanlabs.com>
2015-12-09 21:16:47 +0000

    Moved all auth functions into auth_controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -24,16 +24,53 @@ defmodule Gts.AuthController do
   end
 
   def callback(%{ assigns: %{ ueberauth_auth: auth } } = conn, params) do
-    case User.find_or_create(auth) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "Successfully authenticated.")
-        |> put_session(:current_user, user)
-        |> redirect(to: "/")
-      {:error, reason} ->
-        conn
-        |> put_flash(:error, reason)
-        |> redirect(to: "/")
+    conn
+    |> handle_callback(get_user_from_github_id(auth.uid), auth)
+    |> redirect(to: "/")
+  end
+
+  defp handle_callback(conn, nil, auth) do
+    user = User.changeset(%User{}, %{
+      name: auth.info.name,
+      github_id: auth.uid,
+      avatar: auth.info.urls.avatar_url,
+      encrypted_token: auth.credentials.token
+    })
+
+    if user.valid? do
+      conn
+      |> put_user_in_session(Repo.insert!(user))
+    else
+      conn
+      |> put_flash(:error, "Could not sign in using GitHub.")
     end
+  end
+
+  defp handle_callback(conn, user, auth) do
+    user_changeset = User.changeset(user, %{
+      encrypted_token: auth.credentials.token
+    })
+
+    if user_changeset.valid? do
+      conn
+      |> put_user_in_session(Repo.update!(user_changeset))
+    else
+      conn
+      |> put_flash(:error, "Signed in, but could not update GitHub Access token.")
+    end
+  end
+
+  defp put_user_in_session(conn, user) do
+    conn
+    |> put_session(:current_user, user)
+    |> put_flash(:info, "Successfully authenticated.")
+  end
+
+  defp get_user_from_github_id(github_id) do
+    Repo.one(from u in User, where: u.github_id == ^github_id)
+  end
+
+  defp create_user_from_auth(auth) do
+
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,9 +1,6 @@
 defmodule Gts.User do
   use Gts.Web, :model
 
-  import Ecto.Query
-
-  alias Gts.Repo
   alias Ueberauth.Auth
   alias Ueberauth.Auth.Credentials
 
@@ -29,42 +26,5 @@ defmodule Gts.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
-  end
-
-  # Move to auth controller
-  def find_or_create(%Auth{} = auth) do
-    case find_user_by_github_id(auth.uid) do
-      nil ->
-        user_changeset = changeset(%Gts.User{}, %{
-          name: auth.info.name,
-          github_id: auth.uid,
-          avatar: auth.info.urls.avatar_url,
-          encrypted_token: auth.credentials.token
-        })
-
-        if user_changeset.valid? do
-          {:ok, Repo.insert!(user_changeset)}
-        else
-          {:error, 'Could not sign in using GitHub.'}
-        end
-      user -> {:ok, user}
-    end
-  end
-
-  def find_user_by_github_id(github_id) do
-    query = from u in Gts.User,
-          where: u.github_id == ^github_id,
-         select: u
-    Repo.one(query)
-  end
-
-  defp name_from_auth(auth) do
-    if auth.info.name do
-      auth.info.name
-    else
-      name = [auth.info.first_name, auth.info.last_name]
-      |> Enum.filter(&(&1 != nil and &1 != ""))
-      if length(name) == 0, do: auth.info.nickname, else: name = Enum.join(name, " ")
-    end
   end
 end
